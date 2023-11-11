5e9c15a351d249b585bf72f85725f0bfaa142ba7
Yoshinao Inoguchi <yoshinao@inoguchi.com>
2023-11-11 22:14:57 +0900

    UI を実装

--- yyTodoMailSenderWpf/MainWindow.xaml.cs
+++ yyTodoMailSenderWpf/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using yyGptLib;
 
 namespace yyTodoMailSenderWpf
@@ -9,9 +11,78 @@ namespace yyTodoMailSenderWpf
     /// </summary>
     public partial class MainWindow: Window
     {
-        public MainWindow ()
+        public MainWindow () => InitializeComponent ();
+
+        private void Window_Loaded (object sender, RoutedEventArgs e)
         {
-            InitializeComponent ();
+            try
+            {
+                UpdateTextOfSenderAndRecipient ();
+                UpdateIsEnabledOfSendAndTranslate ();
+                SetInitialFocus ();
+            }
+
+            catch (Exception xException)
+            {
+                SimpleLogger.LogException (xException);
+            }
+        }
+
+        private void Subject_TextChanged (object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            try
+            {
+                UpdateIsEnabledOfSendAndTranslate ();
+            }
+
+            catch (Exception xException)
+            {
+                SimpleLogger.LogException (xException);
+            }
+        }
+
+        private void Body_TextChanged (object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            try
+            {
+                UpdateIsEnabledOfSendAndTranslate ();
+            }
+
+            catch (Exception xException)
+            {
+                SimpleLogger.LogException (xException);
+            }
+        }
+        private void Close_Click (object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Close ();
+            }
+
+            catch (Exception xException)
+            {
+                SimpleLogger.LogException (xException);
+            }
+        }
+
+        private void Window_Closing (object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            try
+            {
+                if (IsEditing)
+                {
+                    MessageBoxResult xResult = MessageBox.Show (this, "Are you sure you want to close this window?", "Confirm", MessageBoxButton.YesNo, MessageBoxImage.Question, MessageBoxResult.No);
+
+                    if (xResult == MessageBoxResult.No)
+                        e.Cancel = true;
+                }
+            }
+
+            catch (Exception xException)
+            {
+                SimpleLogger.LogException (xException);
+            }
         }
     }
 }
---
+++ yyTodoMailSenderWpf/MainWindowHelper.cs
@@ -0,0 +1,53 @@
+﻿using System.Windows;
+
+namespace yyTodoMailSenderWpf
+{
+    public partial class MainWindow: Window
+    {
+        // Data binding doesnt like me.
+        // A lot of code that is copied from the internet or ChatGPT fails to run.
+        // Maybe it's me, maybe it's the IDE.
+        // Sometimes, even code that is suggested by IntelliSense and updates the window preview is rejected by the compiler and there's really nothing I can do about it.
+        // For WPF, I will stick to the good old event-driven programming.
+
+        // In alphabetical order.
+        // KISS.
+
+        private void DisableSubjectAndBody ()
+        {
+            Subject.IsEnabled = false;
+            Body.IsEnabled = false;
+        }
+
+        private void EnableSubjectAndBody ()
+        {
+            Subject.IsEnabled = true;
+            Body.IsEnabled = true;
+        }
+
+        private bool IsEditing => string.IsNullOrWhiteSpace (Subject.Text) == false || string.IsNullOrWhiteSpace (Body.Text) == false;
+
+        private void SetInitialFocus () => Subject.Focus ();
+
+        private void UpdateIsEnabledOfSendAndTranslate ()
+        {
+            if (string.IsNullOrWhiteSpace (Subject.Text) && string.IsNullOrWhiteSpace (Body.Text))
+            {
+                Send.IsEnabled = false;
+                Translate.IsEnabled = false;
+            }
+
+            else
+            {
+                Send.IsEnabled = true;
+                Translate.IsEnabled = true;
+            }
+        }
+
+        private void UpdateTextOfSenderAndRecipient ()
+        {
+            Sender.Text = App.SenderString;
+            Recipient.Text = App.RecipientString;
+        }
+    }
+}
