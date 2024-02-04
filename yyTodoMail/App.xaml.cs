4e01e9becb9a083e00f61650fd54ed4244ce1245
Yoshinao Inoguchi <yoshinao@inoguchi.com>
2024-02-04 12:14:19 +0900

    Added README.md

--- yyTodoMail/App.xaml.cs
+++ yyTodoMail/App.xaml.cs
@@ -97,8 +97,18 @@ namespace yyTodoMail
             {
                 var xGptChatConnectionInfo = new yyGptChatConnectionInfoModel
                 {
+                    // We cant provide a default API key.
+                    // A dummy value is initially contained in the newly generated JSON file.
+                    // The user may: 1) Replace it with a valid key,
+                    //     or 2) Remove the corresponding entry from the file to use the one that MAY be in the .yyUserSecrets file IF it exists and contains the key and a value.
                     ApiKey = "API_KEY",
+
+                    // Rather than a dummy value, the value in the .yyUserSecrets file (IF it's available) or the embedded-in-code default value is initially output.
+                    // The user may: 1) Replace the one in GptChatConnection.json, or 2) Remove the key to use whatever yyGptChat.DefaultEndpoint may return.
                     Endpoint = yyGptChat.DefaultEndpoint
+
+                    // The code above is merely initialization of the JSON file.
+                    // The loading and usage of alternative values are done about 10 lines later.
                 };
 
                 File.WriteAllText (xGptChatConnectionInfoFilePath, JsonSerializer.Serialize (xGptChatConnectionInfo, yyJson.DefaultSerializationOptions), Encoding.UTF8);
@@ -106,7 +116,18 @@ namespace yyTodoMail
                 return null;
             }
 
-            return JsonSerializer.Deserialize <yyGptChatConnectionInfoModel> (File.ReadAllText (xGptChatConnectionInfoFilePath, Encoding.UTF8), yyJson.DefaultDeserializationOptions);
+            var xConnectionInfo = JsonSerializer.Deserialize <yyGptChatConnectionInfoModel> (File.ReadAllText (xGptChatConnectionInfoFilePath, Encoding.UTF8), yyJson.DefaultDeserializationOptions);
+
+            if (xConnectionInfo != null)
+            {
+                if (string.IsNullOrWhiteSpace (xConnectionInfo.ApiKey))
+                    xConnectionInfo.ApiKey = yyGpt.DefaultApiKey;
+
+                if (string.IsNullOrWhiteSpace (xConnectionInfo.Endpoint))
+                    xConnectionInfo.Endpoint = yyGptChat.DefaultEndpoint;
+            }
+
+            return xConnectionInfo;
         });
 
         public static yyGptChatConnectionInfoModel? GptChatConnectionInfo => _gptChatConnectionInfo.Value;
@@ -114,8 +135,14 @@ namespace yyTodoMail
         private static readonly Lazy <yyGptChatConversation> _conversation = new (() =>
         {
             yyGptChatConversation xConversation = new (GptChatConnectionInfo!);
-            xConversation.Request.Model = Shared.AppSpecificConfig ["OpenAiChatModel"].WhiteSpaceToNull () ?? "gpt-4";
+
+            // The choice of model is not exactly connection-related and is exclusively stored in appsettings.json.
+            // If the file contains a valid value, it's used.
+            // If not, as usual, the .yyUserSecrets file is first checked and, if it doesnt exist or doesnt contain the corresponding value, the embedded-in-code default value is used.
+            xConversation.Request.Model = Shared.AppSpecificConfig ["OpenAiChatModel"].WhiteSpaceToNull () ?? yyGptChat.DefaultModel;
+
             xConversation.Request.AddMessage (yyGptChatMessageRole.System, "You are a helpful assistant.");
+
             return xConversation;
         });
 
