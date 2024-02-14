5e8eda11a2396c2e84792e6e0ef6d41aaf9ce862
Yoshinao Inoguchi <yoshinao@inoguchi.com>
2024-02-14 13:45:09 +0900

    Implement UI-related parts

--- yyTodoMail/ViewModels/MainWindowViewModel.cs
+++ yyTodoMail/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,118 @@
-﻿namespace yyTodoMail.ViewModels;
+﻿using System.Reactive;
+using ReactiveUI;
+
+namespace yyTodoMail.ViewModels;
 
 public class MainWindowViewModel: ViewModelBase
 {
+    private string? _sender;
+
+    public string? Sender
+    {
+        get => _sender;
+        set => this.RaiseAndSetIfChanged (ref _sender, value);
+    }
+
+    private string? _subject;
+
+    public string? Subject
+    {
+        get => _subject;
+
+        set
+        {
+            this.RaiseAndSetIfChanged (ref _subject, value);
+            UpdateHasSubjectOrBody ();
+        }
+    }
+
+    private string? _body;
+
+    public string? Body
+    {
+        get => _body;
+
+        set
+        {
+            this.RaiseAndSetIfChanged (ref _body, value);
+            UpdateHasSubjectOrBody ();
+        }
+    }
+
+    private string? _recipient;
+
+    public string? Recipient
+    {
+        get => _recipient;
+        set => this.RaiseAndSetIfChanged (ref _recipient, value);
+    }
+
+    private string? _translatedSubject;
+
+    public string? TranslatedSubject
+    {
+        get => _translatedSubject;
+        set
+        {
+            this.RaiseAndSetIfChanged (ref _translatedSubject, value);
+            UpdateHasTranslation ();
+        }
+    }
+
+    private string? _translatedBody;
+
+    public string? TranslatedBody
+    {
+        get => _translatedBody;
+        set
+        {
+            this.RaiseAndSetIfChanged (ref _translatedBody, value);
+            UpdateHasTranslation ();
+        }
+    }
+
+    // -----------------------------------------------------------------------------
+
+    private bool _hasSubjectOrBody;
+
+    public bool HasSubjectOrBody
+    {
+        get => _hasSubjectOrBody;
+        set => this.RaiseAndSetIfChanged (ref _hasSubjectOrBody, value);
+    }
+
+    public void UpdateHasSubjectOrBody () => HasSubjectOrBody = string.IsNullOrWhiteSpace (Subject) == false || string.IsNullOrWhiteSpace (Body) == false;
+
+    private bool _hasTranslation;
+
+    public bool HasTranslation
+    {
+        get => _hasTranslation;
+        set => this.RaiseAndSetIfChanged (ref _hasTranslation, value);
+    }
+
+    public void UpdateHasTranslation () => HasTranslation = string.IsNullOrWhiteSpace (TranslatedSubject) == false || string.IsNullOrWhiteSpace (TranslatedBody) == false;
+
+    // -----------------------------------------------------------------------------
+
+    public ReactiveCommand <Unit, Unit> SendCommand { get; }
+
+    public ReactiveCommand <Unit, Unit> TranslateCommand { get; }
+
+    public ReactiveCommand <Unit, Unit> SendTranslatedCommand { get; }
+
+    public MainWindowViewModel ()
+    {
+        SendCommand = ReactiveCommand.Create (() =>
+        {
+        });
+
+        TranslateCommand = ReactiveCommand.Create (() =>
+        {
+        });
+
+        SendTranslatedCommand = ReactiveCommand.Create (() =>
+        {
+        });
+    }
 }
--- yyTodoMail/Views/MainWindow.axaml.cs
+++ yyTodoMail/Views/MainWindow.axaml.cs
@@ -1,4 +1,8 @@
-﻿using Avalonia.Controls;
+﻿using System;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using yyLib;
+using yyTodoMail.ViewModels;
 
 namespace yyTodoMail.Views;
 
@@ -7,5 +11,81 @@ public partial class MainWindow: Window
     public MainWindow ()
     {
         InitializeComponent ();
+
+        Opened += (sender, e) =>
+        {
+            try
+            {
+                string? xTitle = Shared.AppSpecificConfig ["Title"];
+
+                if (string.IsNullOrWhiteSpace (xTitle) == false)
+                    Title = xTitle;
+
+                if (bool.TryParse (Shared.AppSpecificConfig ["IsWindowMaximized"], out bool xIsMaximized) && xIsMaximized)
+                    WindowState = WindowState.Maximized;
+
+                string? xFontFamily = Shared.AppSpecificConfig ["FontFamily"];
+
+                if (string.IsNullOrWhiteSpace (xFontFamily) == false)
+                    FontFamily = new (xFontFamily);
+
+                if (double.TryParse (Shared.AppSpecificConfig ["ContentFontSize"], out double xContentFontSize))
+                {
+                    BodyTextBox.FontSize = xContentFontSize;
+                    TranslatedBodyTextBox.FontSize = xContentFontSize;
+                }
+
+                // -----------------------------------------------------------------------------
+
+                SenderTextBox.Text = Session.SenderString;
+                RecipientTextBox.Text = Session.RecipientString;
+
+                // -----------------------------------------------------------------------------
+
+                BodyTextBox.Focus ();
+            }
+
+            catch (Exception xException)
+            {
+                yySimpleLogger.Default.TryWriteException (xException);
+                MessageBox.ShowException (this, xException);
+            }
+        };
+
+        Closing += (sender, e) =>
+        {
+            try
+            {
+                if (DataContext is MainWindowViewModel xViewModel)
+                {
+                    if (xViewModel.HasSubjectOrBody || xViewModel.HasTranslation)
+                    {
+                        if (MessageBox.Show (this, "Are you sure you want to close the window?", "Confirmation",
+                                MessageBoxButton.OKCancel, MessageBoxResult.Cancel) == MessageBoxResult.Cancel)
+                            e.Cancel = true;
+                    }
+                }
+            }
+
+            catch (Exception xException)
+            {
+                yySimpleLogger.Default.TryWriteException (xException);
+                MessageBox.ShowException (this, xException);
+            }
+        };
+    }
+
+    private void CloseButtonClicked (object? sender, RoutedEventArgs e)
+    {
+        try
+        {
+            Close ();
+        }
+
+        catch (Exception xException)
+        {
+            yySimpleLogger.Default.TryWriteException (xException);
+            MessageBox.ShowException (this, xException);
+        }
     }
 }
