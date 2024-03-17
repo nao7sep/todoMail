890de6c5b0e8d70e015cd24e4dc8b1e95e92d14e
Yoshinao Inoguchi <yoshinao@inoguchi.com>
2024-03-17 10:25:59 +0900

    Rename .yyUserSecrets to .yyUserSecrets.json

--- yyTodoMail/Session.cs
+++ yyTodoMail/Session.cs
@@ -96,10 +96,10 @@ namespace yyTodoMail
                     // We cant provide a default API key.
                     // A dummy value is initially contained in the newly generated JSON file.
                     // The user may: 1) Replace it with a valid key,
-                    //     or 2) Remove the corresponding entry from the file to use the one that MAY be in the .yyUserSecrets file IF it exists and contains the key and a value.
+                    //     or 2) Remove the corresponding entry from the file to use the one that MAY be in the .yyUserSecrets.json file IF it exists and contains the key and a value.
                     ApiKey = "API_KEY",
 
-                    // Rather than a dummy value, the value in the .yyUserSecrets file (IF it's available) or the embedded-in-code default value is initially output.
+                    // Rather than a dummy value, the value in the .yyUserSecrets.json file (IF it's available) or the embedded-in-code default value is initially output.
                     // The user may: 1) Replace the one in GptChatConnection.json, or 2) Remove the key to use whatever yyGptChat.DefaultEndpoint may return.
                     Endpoint = yyGptChat.DefaultEndpoint
 
@@ -134,7 +134,7 @@ namespace yyTodoMail
 
             // The choice of model is not exactly connection-related and is exclusively stored in appsettings.json.
             // If the file contains a valid value, it's used.
-            // If not, as usual, the .yyUserSecrets file is first checked and, if it doesnt exist or doesnt contain the corresponding value, the embedded-in-code default value is used.
+            // If not, as usual, the .yyUserSecrets.json file is first checked and, if it doesnt exist or doesnt contain the corresponding value, the embedded-in-code default value is used.
             xConversation.Request.Model = Shared.AppSpecificConfig ["OpenAiChatModel"].WhiteSpaceToNull () ?? yyGptChat.DefaultModel;
 
             xConversation.Request.AddMessage (yyGptChatMessageRole.System, "You are a helpful assistant.");
